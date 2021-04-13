1125dcdff77408eb96e37838be59a3d4062b3acc
Raghav Tulsyan <raghavtulsyan2010@gmail.com>
2021-04-14 04:29:26 +0530

    test file modify

--- 4. Implementation/test.c
+++ 4. Implementation/test.c
@@ -9,3 +9,18 @@ void tearDown(){
 
 }
 
+void mortgage_test(){
+
+}
+
+void loan_test(){
+
+}
+
+int main()
+{
+    UNITY_BEGIN();
+    RUN_TEST(mortgage_test);
+    RUN_TEST(loan_test);
+    return UNITY_END();
+}
