c2fb18158ffcd6d7bd07c649fb8b70a85c2a1a62
Raghav Tulsyan <raghavtulsyan2010@gmail.com>
2021-04-15 00:45:47 +0530

    multi file created

--- 3_ Implementation/mortgage.c
+++ 3_ Implementation/mortgage.c
@@ -56,30 +56,3 @@ int mortgage(){
     return 0;
 
 }
-
-int loan(){
-    float principal;
-    float annualInterestRate;
-    int years;
-
-    printf("\nLoan Amount: ");
-    scanf("%f",&principal);
-    printf("\nPeriod (Years): ");
-    scanf("%d",&years);
-    printf("\nAnnual interest Rate: ");
-    scanf("%f",&annualInterestRate);
-    
-    float i = ((annualInterestRate)/(100))/12;
-    int months = years*12;
-
-    float Discount = (pow((1+i),months)-1) / ((i)*pow((1+i),months));
-    float loanPaymentAmount = (principal)/(Discount);
-
-    printf("\n");
-    printf("Loan Details\n");
-    printf("------------\n");
-    printf("\nDiscount: %.2f\n",Discount);
-    printf("\nMonthly Loan Payment: %.2f\n",loanPaymentAmount);
-
-    return 0;
-}
